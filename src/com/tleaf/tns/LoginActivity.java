a504f7471421144dce3914f3ad69cb8f33ced046
SeulGiChoi <abooun1011@gmail.com>
2014-11-13 16:34:34 +0900

    tns ui, shack singin, singup ui

---
+++ src/com/tleaf/tns/LoginActivity.java
@@ -0,0 +1,53 @@
+package com.tleaf.tns;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        
+        TextView txt_singup = (TextView) findViewById(R.id.txt_singup);
+        txt_singup.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent;
+				intent = new Intent(LoginActivity.this,
+						SingUpActivity.class);
+				startActivity(intent);
+				finish();					
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
